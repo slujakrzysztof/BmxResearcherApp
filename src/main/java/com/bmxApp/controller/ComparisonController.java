ccebfb4d0b920532b85a6c57f145ab48e2ddc81b
slujakrzysztof <krzysiek.sluja@vp.pl>
2023-04-11 20:59:57 +0200

    Refactor comparison controller and service

--- src/main/java/com/bmxApp/controller/ComparisonController.java
+++ src/main/java/com/bmxApp/controller/ComparisonController.java
@@ -3,17 +3,15 @@ package com.bmxApp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.bmxApp.creator.PathCreator;
 import com.bmxApp.dto.product.CompareProductDTO;
-import com.bmxApp.dto.product.ProductDTO;
 import com.bmxApp.service.comparison.ComparisonService;
 
 import jakarta.annotation.Nullable;
@@ -26,26 +24,30 @@ public class ComparisonController {
 
 	private final ComparisonService comparisonControllerService;
 
-	@RequestMapping(value = "/compare", method = { RequestMethod.POST, RequestMethod.GET })
-	public String compare(@Nullable @ModelAttribute("compareProduct") CompareProductDTO product, Model model,
-			HttpServletRequest request) {
+	@PostMapping(value = "/compare")
+	public RedirectView compare(@Nullable @ModelAttribute("compareProduct") CompareProductDTO product,
+			@RequestParam("currentUrl") String currentUrl) {
 
-		System.out.println("PROD: " + product.toString());
-		
-		if(product != null) comparisonControllerService.compare(product);
-		
+		comparisonControllerService.compare(product);
+
+		return new RedirectView(currentUrl);
+	}
+
+	@GetMapping(value = "/comparator")
+	public String showComparator(Model model, HttpServletRequest request) {
+
 		model.addAttribute("compareProduct", comparisonControllerService.getCompareProducts());
 		model.addAttribute("currentUrl", PathCreator.getCurrentUrl(request));
+
 		return "comparator";
 	}
 
 	@DeleteMapping("/deleteCompareProduct/{id}")
-	public RedirectView deleteFromComparator(@PathVariable("id") int id, @RequestParam("currentUrl") String currentUrl,
-			Model model) {
+	public RedirectView deleteFromComparator(@PathVariable("id") int id,
+			@RequestParam("currentUrl") String currentUrl) {
 
 		comparisonControllerService.deleteProduct(id);
 
-		model.addAttribute("compareProduct", comparisonControllerService.getCompareProducts());
 		return new RedirectView(currentUrl);
 	}
 }
--- src/main/java/com/bmxApp/service/comparison/ComparisonService.java
+++ src/main/java/com/bmxApp/service/comparison/ComparisonService.java
@@ -1,14 +1,20 @@
 package com.bmxApp.service.comparison;
 
 import java.math.BigDecimal;
+import java.net.http.HttpRequest;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.collections4.EnumerationUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.util.EnumUtils;
 
 import com.bmxApp.dto.product.CompareProductDTO;
 import com.bmxApp.researcher.ShopResearcherService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -16,72 +22,79 @@ import lombok.Setter;
 @Service
 @Getter
 @Setter
-//@RequiredArgsConstructor
 public class ComparisonService {
 
 	private final CompareProductDTO NULL_PRODUCT;
-	
+	private final String HEADER;
+
 	private boolean productOneAdded;
 	private boolean productTwoAdded;
 	private Map<Integer, CompareProductDTO> compareProducts;
 	private final ShopResearcherService shopResearcherService;
 	private String currentUrl;
-	
-	
+
 	public ComparisonService(ShopResearcherService shopResearcherService) {
-		
+
 		this.shopResearcherService = shopResearcherService;
-		NULL_PRODUCT = CompareProductDTO.builder()
-				.productName("").shopName("")
-				.price(new BigDecimal(0)).description("")
-				.uri("").imageUrl(null)
-				.build();
+		NULL_PRODUCT = CompareProductDTO.builder().productName("").shopName("").price(new BigDecimal(0)).description("")
+				.uri("").imageUrl(null).build();
+		HEADER = "productDeleted";
 		productOneAdded = false;
 		productTwoAdded = false;
 		compareProducts = new HashMap<>();
 	}
-	
+
 	private CompareProductDTO createCompareProduct(CompareProductDTO product) {
-		
+
 		CompareProductDTO compareProduct = product;
 		String description = shopResearcherService.getCompareDescription(product.getUri());
-		
+
 		compareProduct.setDescription(description);
-		
+
 		return compareProduct;
-		
+
 	}
 
 	public void compare(CompareProductDTO product) {
-		
-		if(productOneAdded && !productTwoAdded) {
+
+		if (product.getProductName() != null) {
 			
-			compareProducts.put(2, this.createCompareProduct(product));
-			setProductTwoAdded(true);
+			if (productOneAdded && !productTwoAdded) {
+
+				compareProducts.put(2, this.createCompareProduct(product));
+				setProductTwoAdded(true);
+			} else if (!productOneAdded && !productTwoAdded) {
+
+				compareProducts.put(1, this.createCompareProduct(product));
+				compareProducts.put(2, NULL_PRODUCT);
+
+				setProductOneAdded(true);
+			} else if (productOneAdded && productTwoAdded) {
+
+				setProductOneAdded(false);
+				compare(product);
+			} else if (!productOneAdded && productTwoAdded) {
+
+				compareProducts.put(1, this.createCompareProduct(product));
+				setProductOneAdded(true);
+				setProductTwoAdded(false);
+			}
 		}
-		else if (!productOneAdded && !productTwoAdded) {
-			
-			compareProducts.put(1, this.createCompareProduct(product));
-			compareProducts.put(2, NULL_PRODUCT);
-			
-			setProductOneAdded(true);
-		}
-		else if(productOneAdded && productTwoAdded) {
-			
+	}
+
+	public void deleteProduct(int id) {
+
+		compareProducts.put(id, NULL_PRODUCT);
+
+		switch (id) {
+
+		case 1: {
 			setProductOneAdded(false);
-			compare(product);
+			break;
 		}
-		else if(!productOneAdded && productTwoAdded) {
-			
-			compareProducts.put(1, this.createCompareProduct(product));
-			setProductOneAdded(true);
+		case 2:
 			setProductTwoAdded(false);
 		}
-				
 	}
-	
-	public void deleteProduct(int id) {
-		
-		compareProducts.put(id, NULL_PRODUCT);
-	}
+
 }
